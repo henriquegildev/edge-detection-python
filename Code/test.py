9a20e615b97ea4594e21e31df353027259506d73
henriquegildev <74499230+henriquegildev@users.noreply.github.com>
2022-01-21 17:31:13 +0000

    Added Crossover function, improved selecionar_da_populacai
    Added output to file.

--- Code/test.py
+++ Code/test.py
@@ -1,12 +1,18 @@
+import datetime
+import random
+import sys
+import traceback
 from random import choices
 from random import uniform
-from operator import itemgetter
+from datetime import datetime
 import time
 import numpy as np
 import glob
 import scipy.ndimage
 import imageio
 
+
+randint = random.randint
 start_time = time.time()
 
 
@@ -65,6 +71,12 @@ def gerar_populacao(n):
     return [gerar_genoma() for _ in range(n)]
 
 
+def encher_populacao(n, populacao):
+    for i in range(n):
+        populacao.append(gerar_genoma())
+    return populacao
+
+
 """
 Nome: Selecionar da população
 Desc.: Seleciona da população o top 10%
@@ -72,65 +84,124 @@ Input: populacao -> list(genoma); comparar_imagens -> original_image, processed_
 """
 
 
-def selecionar_da_populacao(populacao, comparar_imagens, perf, process):
-    return choices(
-        population=populacao,
-        weights=[comparar_imagens(perf, process) for genoma in populacao],
-        k=0.1 * len(populacao)
-    )
+def selecionar_da_populacao(n: int, populacao: list):
+    new_pop = []
+    populacao.sort()
+    new_pop_temp = populacao[:int(n * 0.1)]
+    for i in new_pop_temp:
+        new_pop.append(i[1])
+    # print(new_pop)
+    print("Old population size: {} ~ New population size: {}".format(len(populacao), len(new_pop)))
+    return new_pop
 
+
+"""
+Nome: Crossover
+Desc.: Seleciona da população o top 10%
+Input: populacao -> list(genoma); comparar_imagens -> original_image, processed_image; original_image -> np.array(float64); processed_image -> np.array(float64)
+"""
+
+#Implementar evolucao da funcao de custo
+#Crossover fazer de 1-5 com distribuicao centrada no 3
+#Fazer swap fixo, mas quais sao
+def crossover(populacao: list):
+    combinations = {}
+    new_pop = []
+    index = 0
+    for i in populacao:
+        combinations[index] = []
+        pos = randint(0, len(populacao) - 1)
+        try:
+            if pos in combinations:
+                while index in combinations[pos]:
+                    pos = randint(0, len(populacao) - 1)
+        except KeyError as err:
+            print("Something happened: ", err)
+            traceback.print_exc()
+        combinations[index].append(pos)
+        rand = populacao[pos]
+        g1_a, g1_b, g2_a, g2_b = list(i[0]), list(i[2]), list(rand[0]), list(rand[2])
+        list_genesA, list_genesB = [g1_a, g2_a], [g1_b, g2_b]
+        listA, listB = [[], []], [list_genesA, list_genesB]
+        for par in range(0, 2):
+            for x in range(0, 3):
+                pos1 = randint(0, len(list_genesA) - 1)
+                gene = listB[par][pos1]
+                pos2 = randint(0, len(gene) - 1)
+                chr = gene[pos2]
+                listA[par].append(chr)
+        new_gene = np.array([listA[0], [0, 0, 0], listA[1]])
+        new_pop.append(new_gene)
+        index += 1
+    # print(new_pop)
+    return new_pop
 
 if __name__ == '__main__':
-    # lista_de_treino = buscar_lista_de_treino(DefinicoesClassificador.src_path_original)
-    # print("Original Photos Paths: ", DefinicoesClassificador.src_path_original)
-    # print("Perfect Photos Paths: ", DefinicoesClassificador.src_path_perfect)
+    index = 0
+    num_file = len(glob.glob(DefinicoesClassificador.src_path_results_text + 'results*.txt'))
+    output_file = open(DefinicoesClassificador.src_path_results_text + "results{}.txt".format(num_file + 1), 'w')
     lista_de_treino = DefinicoesClassificador.src_path_original
-    lista_matrizes_precisas = [[]]
-    for ficheiro_de_imagem in lista_de_treino:
-        index = lista_de_treino.index(ficheiro_de_imagem)
-        print("Index: ", index, " File: ", ficheiro_de_imagem)
+    iterations = 10
 
-        # Leitura de imagem a processar, resultando num np.array
-        imagem = imageio.imread(ficheiro_de_imagem)
-        imagem_float = np.array(imagem, dtype=np.float64)  # Converter data type a float64
+    populacao = gerar_populacao(10)
+    size_of_pop = len(populacao)
+    output_file.write(
+        "Run Begin Time: {} | Run Number: {} | Projected Iterations: {} | Size of Population: {}\n".format(datetime.now(),
+                                                                                                         num_file,
+                                                                                                         iterations,
+                                                                                                         size_of_pop))
+    for i in range(0, iterations):
+        print("New Cycle - Iteration: {}/{}".format(i + 1, iterations))
+        media_val = []
+        populacao_results = []
+        for ficheiro_de_imagem in lista_de_treino:
+            lista_path = ficheiro_de_imagem.split('\\')
+            nome = lista_path[-1]
+            index = lista_de_treino.index(ficheiro_de_imagem)
+            print("Iterations: {}/{} | Index: {}/200 | File: {}".format(i + 1, iterations, index, nome))
 
-        lista_path = ficheiro_de_imagem.split('\\')
-        nome = lista_path[-1]
-        # Leitura de imagem perfeita, resultando num np.array
-        imagem_perfeita = imageio.imread(DefinicoesClassificador.src_path_perfect[index])
-        imagem_perfeita_float = np.array(imagem_perfeita, dtype=np.float64)
-        lista_distancias = []
-        lista_genomas = []
-        # Aplicação de genoma
-        # Time Complexity O(n)
-        for genoma in gerar_populacao(1000):
-            # Associar genomas a variáveis, sendo a Y transposta
-            # hX = np.array([[-1.0, -2.0, -1.0], [0, 0, 0], [1.0, 2.0, 1.0]])
-            hX = genoma
-            hY = np.transpose(hX)
+            # Leitura de imagem a processar, resultando num np.array
+            imagem = imageio.imread(ficheiro_de_imagem)
+            imagem_float = np.array(imagem, dtype=np.float64)  # Converter data type a float64
 
-            gX = scipy.ndimage.convolve(imagem_float, hX)
-            gY = scipy.ndimage.convolve(imagem_float, hY)
-            g = np.abs(gX + gY)
-            g_normal = g * (255.0 / (scipy.ndimage.maximum(g) - scipy.ndimage.minimum(g)))
 
-            distancia = comparar_imagens(imagem_perfeita_float, g_normal)
-            lista_genomas.append(hX)
-            lista_distancias.append(distancia)
-        # print(scipy.ndimage.minimum(g_normal), scipy.ndimage.maximum(g_normal))
+            # Leitura de imagem perfeita, resultando num np.array
+            imagem_perfeita = imageio.imread(DefinicoesClassificador.src_path_perfect[index])
+            imagem_perfeita_float = np.array(imagem_perfeita, dtype=np.float64)
+            lista_distancias = []
+            lista_genomas = []
+            # Aplicação de genoma
+            # Time Complexity O(n)
+            for genoma in populacao:
+                # Associar genomas a variáveis, sendo a Y transposta
+                # hX = np.array([[-1.0, -2.0, -1.0], [0, 0, 0], [1.0, 2.0, 1.0]])
+                hX = genoma
+                hY = np.transpose(hX)
 
-        imageio.imwrite(DefinicoesClassificador.src_path_results + 'processed_{}'.format(nome),
-                        g_normal.astype(np.uint8))
-        # print(imagem.shape)
-        # imageio.imwrite(DefinicoesClassificador.src_path_results + 'processed_{}'.format(nome),g_normal.astype(np.uint8))
-        #print(nome, "| distancia: ", distancia)
-        minimum = min(lista_distancias)
-        pos = lista_distancias.index(minimum)
-        lista_matrizes_precisas.append([minimum,lista_genomas[pos]])
-        print("Minimum: ", minimum, "\n", "Matrix: ", lista_genomas[pos])
-        print("--- Elapsed Time: %s seconds ---" % (time.time() - start_time))
-        # print(imagem.shape)
-lista_matrizes_precisas.remove([])
-lista_matrizes_precisas.sort()
-print(lista_matrizes_precisas[:1])
+                gX = scipy.ndimage.convolve(imagem_float, hX)
+                gY = scipy.ndimage.convolve(imagem_float, hY)
+                g = np.abs(gX + gY)
+                g_normal = g * (255.0 / (scipy.ndimage.maximum(g) - scipy.ndimage.minimum(g)))
+
+                distancia = comparar_imagens(imagem_perfeita_float, g_normal)
+                lista_genomas.append(hX)
+                lista_distancias.append(distancia)
+
+            imageio.imwrite(DefinicoesClassificador.src_path_results + 'processed_{}'.format(nome),
+                            g_normal.astype(np.uint8))
+            minimum = min(lista_distancias)
+            media_val.append(minimum)
+            pos = lista_distancias.index(minimum)
+            populacao_results.append([minimum, lista_genomas[pos]])
+            # print("Minimum: ", minimum, "\n", "Matrix: ", lista_genomas[pos])
+            print(" Elapsed Time: {:4f} seconds | Minimum: {}".format(time.time() - start_time, minimum))
+            print("Average Fitness: {}".format(sum(media_val) / len(media_val)))
+            output_file.write(
+                "Average Fitness: {} | Iteration: {}/{} | File: {} \n".format((sum(media_val) / len(media_val)), i + 1, iterations, nome))
+        populacao_tmp = selecionar_da_populacao(size_of_pop, populacao_results)
+        populacao = crossover(populacao_tmp)
+        encher_populacao(int(0.9 * size_of_pop), populacao)
+
+output_file.write("--- Final Time: {:4f} seconds ---".format(time.time() - start_time))
+output_file.close()
 print("--- Final time: %s seconds ---" % (time.time() - start_time))
